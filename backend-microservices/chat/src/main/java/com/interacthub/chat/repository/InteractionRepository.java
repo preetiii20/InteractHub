68dfe79d6ce66d0e09879fad91f8eac840efde41
kolipreeti2725@gmail.com <kolipreeti2725@gmail.com>
2025-11-22 19:50:07 +0530

    Clean initial commit with proper .gitignore

--- backend-microservices/chat/src/main/java/com/interacthub/chat/repository/InteractionRepository.java
+++
@@ -1,8 +0,0 @@
-package com.interacthub.chat.repository;
-
-import com.interacthub.chat.model.Interaction;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InteractionRepository extends JpaRepository<Interaction, Long> { }
